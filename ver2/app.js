b1a354553b06cc04aaf2707cb21e0642067c3bb6
Pablo <pablo.garavito@gmail.com>
2023-11-28 19:13:44 -0500

    working on 2nd version, task as object

--- ver2/app.js
+++ ver2/app.js
@@ -1,20 +1,47 @@
+class Task {
+    constructor(text, done) {
+        this.text = text;
+        this.done = done;
+    }
+
+    get isDone() {
+        return this.done;
+    }
+
+    set isDone(value) {
+        this.done = value;
+    }
+}
+
+const testTask = new Task('hello', false);
+console.log(testTask.isDone);
+testTask.done = true;
+console.log(testTask.isDone);
+
+
 const inputBox = document.querySelector('#input-box');
 const taskList = document.querySelector('#task-list');
 
-const addBtn = document.querySelector('add-btn');
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const addBtn = document.querySelector('#add-btn');
+// const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const tasks = [];
 
 addBtn.addEventListener('click', addTask);
 
-const addTask = () => {
+function addTask() {
     const taskText = inputBox.value.trim();
     if (!taskText) return;
 
-    const task = {text: taskText};
+    const task = new Task(taskText, false);
     tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    //localStorage.setItem('tasks', JSON.stringify(tasks));
     inputBox.value = '';
     displayTasks();
+}
+
+function checkTask(index) {
+    let done = tasks[index].done;
+    tasks[index].done = !done;
 }
 
 const displayTasks = () => {
@@ -23,13 +50,41 @@ const displayTasks = () => {
     tasks.forEach((task, index) => {
         const li = document.createElement('li');
         li.innerHTML = `
-            <span>${task.text}</span>
-            <hr>
-            <input type="image" class="img-button check-button" src="res/check.png" onclick="checkTask(${index})" />
-            <input type="image" class="img-button edit-button" src="res/edit.png" onclick="editTask(${index})" />
-            <input type="image" class="img-button delete-button" src="res/remove.png" onclick="deleteTask(${index})" /> 
+            <span onclick="checkTask(${index})>${task.text}</span>
+            
+            <span class="deleteSpan" onclick="deleteTask(${index})">\u00d7</span>
         `;
+        if (task.isDone) {
+            
+        }
+        // li.innerHTML = task.text;
+        // let span = document.createElement('span');
+        // span.innerText = '\u00d7';
+        // li.appendChild(span);
         taskList.appendChild(li);
     });
 }
+
+taskList.addEventListener('click', function(e){
+    console.log('jue');
+    if (e.target.tagName.toUpperCase() === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+});
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    displayTasks();
+}
+
+// const task = new Task('hello', false); 
+
+// console.log(task);
+
+// task.done = true;
+
+// console.log(task);
+
+// displayTasks();
 
