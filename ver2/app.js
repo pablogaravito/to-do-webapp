28aa89bdbb9807b07f774b11b587a251aaa24d33
Pablo <pablo.garavito@gmail.com>
2023-11-28 19:49:27 -0500

    added sort function, 2nd version fully working

--- ver2/app.js
+++ ver2/app.js
@@ -23,8 +23,7 @@ const inputBox = document.querySelector('#input-box');
 const taskList = document.querySelector('#task-list');
 
 const addBtn = document.querySelector('#add-btn');
-// const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-const tasks = [];
+const tasks = JSON.parse(localStorage.getItem('todo')) || [];
 
 addBtn.addEventListener('click', addTask);
 
@@ -34,59 +33,46 @@ function addTask() {
 
     const task = new Task(taskText, false);
     tasks.push(task);
-    //localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('todo', JSON.stringify(tasks));
     inputBox.value = '';
     displayTasks();
 }
 
 function checkTask(index) {
     let done = tasks[index].isDone;
-    // console.log('current status', done);
     tasks[index].done = !done;
     done = tasks[index].isDone;
-    // console.log('new current status', done);
     displayTasks();
 }
 
 const displayTasks = () => {
     taskList.innerHTML = "";
-    // console.log(tasks);
+    
+    tasks.sort((a, b) => a.isDone - b.isDone);
     tasks.forEach((task, index) => {
         const li = document.createElement('li');
-        // console.log(task);
+        
         li.innerHTML = `
             <span onclick="checkTask(${index})">${task.text}</span>
             
             <span class="deleteSpan" onclick="deleteTask(${index})">\u00d7</span>
         `;
         if (task.isDone) {
-            // console.log('done done');
             li.classList.add('checked');
         } else {
             li.classList.remove('checked');
-            // console.log('not done man');
         }
-        // li.innerHTML = task.text;
-        // let span = document.createElement('span');
-        // span.innerText = '\u00d7';
-        // li.appendChild(span);
+
         taskList.appendChild(li);
     });
 }
 
-// taskList.addEventListener('click', function(e){
-//     //console.log('jue');
-//     if (e.target.tagName.toUpperCase() === 'LI') {
-//         e.target.classList.toggle('checked');
-//     }
-// });
 
 function deleteTask(index) {
     tasks.splice(index, 1);
-    // localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('todo', JSON.stringify(tasks));
     displayTasks();
 }
-
 
 displayTasks();
 
